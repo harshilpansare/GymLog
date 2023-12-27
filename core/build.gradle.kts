f9a9f28802a5bb5568049e2804b6279e7d7d9d10
harshil <harshilpansare@gmail.com>
2023-12-27 16:44:01 +0530

    common libraries added

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
 }
 
 android {
@@ -33,11 +35,48 @@ android {
 }
 
 dependencies {
+    api("androidx.core:core-ktx:1.12.0")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:5.0-SNAPSHOT")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Hilt
+    api("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+
+    // Retrofit + gson
+    api("com.squareup.retrofit2:retrofit:2.9.0")
+    api("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    // Gson
+    api("com.google.code.gson:gson:2.10.1")
+    api("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Glide
+    api("com.github.bumptech.glide:glide:4.15.1")
+    kapt("com.github.bumptech.glide:compiler:4.15.1")
+
+    // ViewModel LiveData Lifecycle
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    api("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    // Activity fragment appcompat
+    api("androidx.appcompat:appcompat:1.6.1")
+    api("androidx.activity:activity-ktx:1.8.2")
+    api("androidx.fragment:fragment-ktx:1.6.2")
+    api("androidx.core:core-ktx:1.12.0")
+
+    // Compose
+    api("androidx.activity:activity-compose:1.8.2")
+    api(platform("androidx.compose:compose-bom:2023.08.00"))
+    api("androidx.compose.ui:ui")
+    api("androidx.compose.ui:ui-graphics")
+    api("androidx.compose.ui:ui-tooling-preview")
+    api("androidx.compose.material3:material3")
+    debugApi("androidx.compose.ui:ui-tooling")
+    debugApi("androidx.compose.ui:ui-test-manifest")
+
+    // Coroutines
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // Junit
+    api("junit:junit:5.0-SNAPSHOT")
 }
